f1966370b33d3474229496a1e71a2186cfd86e00
Federico Romairone <federico.romairone.cs@gmail.com>
2024-06-09 18:35:15 +0200

    edit1

--- pages/1_📈_Analisi.py
+++ pages/1_📈_Analisi.py
@@ -2,11 +2,56 @@ import streamlit as st
 from utils.utils import *
 import pandas as pd
 
-#ogni tab ha una funzione separata
+# ogni tab ha una funzione separata
+
+def create_tab_prodotti(tab_prodotti):
+    col1, col2, col3 = tab_prodotti.columns(3)
+    
+    # eseguo query richiesta e salvo l'oggetto risultante
+    query = """
+            SELECT SUM(amount) as TotalAmount, MAX(amount) as MaxAmount, AVG(amount) as AVGAmount 
+            FROM payments;
+            """
+    payment_info = execute_query(st.session_state["connection"], query)
+    
+    # []   --> creo lista 
+    # dict --> di dizionari 
+    # zip  --> di coppie (nome attributo, valore) del risultato della query
+    payment_info_dict = [dict(zip(payment_info.keys(), result)) for result in payment_info]
+
+    # metriche dei prodotti
+    col1.metric("Importo totale: ", f'$ {compact_form(payment_info_dict[0]["TotalAmount"])}')
+    col2.metric("Importo massimo: ", f'$ {compact_form(payment_info_dict[0]["MaxAmount"])}')
+    col3.metric("Importo medio: ", f'$ {compact_form(payment_info_dict[0]["AVGAmount"])}')
+
+    # panoramica dei prodotti
+    with tab_prodotti.expander("Panoramica prodotti", True):
+        query_base = """
+                     SELECT productCode as code, productName as name, quantityInStock as quantity, buyPrice as price, MSRP
+                     FROM products
+                     """
+        # gestione input utente
+        prod_col1, prod_col2 = st.columns([3, 3])
+        sort_param = prod_col1.radio("Ordina per: ", ["code", "name", "quantity", "price"])
+        sort_choice = prod_col2.selectbox("Ordine: ", ["crescente", "decrescente"])
+        sort_dict = {
+            "crescente": "ASC", 
+            "descrescente":"DESC"
+        }
+        query = "%s ORDER BY %s %s" % (query_base, sort_param, sort_dict[sort_choice])
+        # salvo risultato query
+        products = execute_query(st.session_state["connection"], query)
+        # salvo una tabella interattiva pandas
+        products_df = pd.DataFrame(products)
+        # creo un dataframe stramlit
+        st.dataframe(products_df, use_container_width = True)
 
 if __name__ == "__main__":
     st.title("📈 Analisi")
 
-    #creazione dei tab distinti
+    # creazione dei tab distinti
     tab_prodotti,tab_staff,tab_clienti=st.tabs(["Prodotti","Staff","Clienti"])
     
+    # connessione
+    if check_connection():
+        create_tab_prodotti(tab_prodotti)
--- utils/utils.py
+++ utils/utils.py
@@ -2,3 +2,63 @@ import streamlit as st
 from sqlalchemy import create_engine,text
 
 """Raccoglie le principali funzioni condivise dalle varie pagine"""
+
+def connect_db(dialect, username, password, host, dbname):
+    """
+    Crea un connessione al db.
+    
+    :param dialect: specifico modo di comunicare del db
+    :return: False se ci sono eccezioni, connessione al db altrimenti
+    """
+    try:
+        # engine (motore) = punto di collegamento tra app e database
+        engine = create_engine(f'{dialect}://{username}:{password}@{host}/{dbname}')
+        conn = engine.connect()
+        return conn
+    except:
+        return False
+    
+def check_connection():
+    """
+    Crea un button e verifica che sia stata richiesta una connessione al db tramite onclick event.
+    Mosta un messaggio di successo o insuccesso a seconda dell'esito della connessione.
+
+    :return: True per connessione con successo, False altrimenti
+    """
+    # inizializzo variabile per connessione
+    if "connection" not in st.session_state.keys():
+        st.session_state["connection"] = False
+    
+    # creo il bottone e verifico se sia stato cliccato
+    if st.sidebar.button("Connetti al database"):
+        myconnection = connect_db("mysql+pymysql", "student", "user_pwd", "localhost", "classicmodels")
+        if myconnection is not False:
+            st.session_state["connection"] = myconnection
+        else:
+            st.session_state["connection"]=False
+            st.sidebar.error("Errore nella connessione al DB")
+    
+        # se connessione creata, messaggio di successo, altrimenti fallimento
+        if st.session_state["connection"]:
+            st.sidebar.success("Connesso al DB")
+            return True
+
+def execute_query(conn, query):
+    """
+    Esegue una query sul database connesso.
+    """
+    return conn.execute(text(query))
+
+def compact_form(num):
+    """
+    Restituisce un numero dato in forma compatta.
+    """
+    num = float(num)
+    if abs(num) >= 1e9:
+        return "%.2fB" % (num / 1e9)
+    elif abs(num) >= 1e6:
+        return "%.2fM" % (num / 1e6)
+    elif abs(num) >= 1e3:
+        return "%.2fK" % (num / 1e3)
+    else:
+        return "%.2f" % num
